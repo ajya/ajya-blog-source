d32a6d2c4e8afdcfba51930f5d96c4d2752c2c0e
ajya <mail@clusums.eu>
2018-05-12 21:19:53 +0300

    First entry 'Hello Outreachy'

---
+++ content/hello-outreachy.md
@@ -0,0 +1,5 @@
+Title: Hello Outreachy
+Date: 2018-04-29 18:00
+Category: outreachy
+
+Hello World, this is my first blog post for my journey as Outreachy intern at OpenStack "Extend Redfish hardware provisioning tooling for Python" project.
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'ajya'
+SITENAME = u'The Blog'
+SITEURL = 'https://ajya.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Riga'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_DOMAIN = SITEURL
+FEED_ALL_RSS='feeds/all.rss.xml'
+CATEGORY_FEED_RSS='feeds/%s.rss.xml'
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME='Flex'
+
+# Blogroll
+LINKS = (('Outreachy', 'https://www.outreachy.org/'),
+         ('OpenStack', 'https://www.openstack.org'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
